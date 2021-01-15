760610a30046f1657132d7b313fd49be6994222a
Scott <scott@scott-liu.com>
2021-01-15 07:02:57 +0000

    Added training script

---
+++ train.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo TRAINING...
+mkdir -p output
+combine_tessdata -e tessdata/eng.traineddata \
+  ~/tessdata/eng.lstm
+lstmtraining --model_output output \
+  --continue_from tessdata/eng.lstm \
+  --traineddata tessdata/eng.traineddata \
+  --train_listfile training_data/eng.training_files.txt \
+  --max_iterations 400
