7fd47eb76a15cb064f2976c6659cfe1458a455fa
Scott <scott@scott-liu.com>
2021-01-15 07:13:59 +0000

    Added section to save to .traineddata

--- train.sh
+++ train.sh
@@ -3,8 +3,12 @@ echo TRAINING...
 mkdir -p output
 combine_tessdata -e tessdata/eng.traineddata \
   tessdata/eng.lstm
-lstmtraining --model_output output \
+lstmtraining --model_output output/custom \
   --continue_from tessdata/eng.lstm \
   --traineddata tessdata/eng.traineddata \
   --train_listfile training_data/eng.training_files.txt \
-  --max_iterations 400
+  --max_iterations 1000
+lstmtraining --stop_training \
+  --continue_from output/custom_checkpoint \
+  --traineddata tessdata/eng.traineddata \
+  --model_output output/custom.traineddata
