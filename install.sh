bfbe453166adbe7521eab8ebd489cf8af1c5caff
Scott <scott@scott-liu.com>
2021-01-15 06:35:30 +0000

    fixed fontlist argument in install script

--- install.sh
+++ install.sh
@@ -29,4 +29,8 @@ cd ..
 
 echo CREATING TRAINING DATA...
 mkdir training_data
-bash tesstrain.sh --fonts_dir font --lang eng --linedata_only --noextract_font_properties --langdata_dir langdata --tessdata_dir tessdata --output_dir training_data
+fonts="$(text2image --list_available_fonts --fonts_dir font | sed 's/.*: //' | sed 's/.*/"&"/')"
+fonts="$(echo $fonts)"
+echo $fonts
+cmd="bash tesstrain.sh --fonts_dir font --fontlist "$fonts" --lang eng --linedata_only --noextract_font_properties --langdata_dir langdata --tessdata_dir tessdata --output_dir training_data"
+eval $cmd
